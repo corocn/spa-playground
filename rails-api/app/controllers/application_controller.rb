d59c5375c69d9d2e44119275f5b606f32b42e983
corocn <crsk.plmzh@gmail.com>
2021-09-20 22:44:22 +0900

    refactor

--- rails-api/app/controllers/application_controller.rb
+++ rails-api/app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
 
   protect_from_forgery with: :exception
+
+  def set_csrf_token
+    cookies['CSRF-TOKEN'] = {
+      value: form_authenticity_token
+    }
+  end
 end
--- rails-api/app/controllers/sessions_controller.rb
+++ rails-api/app/controllers/sessions_controller.rb
@@ -3,10 +3,4 @@ class SessionsController < ApplicationController
     set_csrf_token
     render json: {}, status: :ok
   end
-
-  def set_csrf_token
-    cookies['CSRF-TOKEN'] = {
-      value: form_authenticity_token
-    }
-  end
 end
